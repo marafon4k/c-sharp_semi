46be09db4ba033c59c5b22ddc4659946cb2cb1ca
Igor Chichinin <marafon4k@gmail.com>
2022-07-08 22:03:34 +0300

    Added second and third work

---
+++ second/Program.cs
@@ -0,0 +1,47 @@
+﻿// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да #
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+// int a = 25,
+//     b = 5,
+//     sum_b = 0;
+
+// sum_b = b * b;
+
+// if a == sum_b {
+//     Console.WriteLine("Yes");
+// } else {
+//     Console.WriteLine("No");
+// }
+
+// Console.WriteLine();
+
+
+// int a = Console.WriteLine("Введите число: ");
+// int b = Console.WriteLine("Введите число: ");
+// int sum_b = b * b;
+
+// if sum_b == a {
+//     Console.WriteLine("Yes");
+// } else {
+//     Console.WriteLine("No");
+// }
+
+Console.WriteLine("Введите целое число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+int result = number_1 / number_2;
+if(result == number_2)
+Console.WriteLine("Yes");
+else Console.WriteLine("No");
---
+++ third/Program.cs
@@ -0,0 +1,25 @@
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Введите день недели в цифре: ");
+int day_week = Convert.ToInt32(Console.ReadLine());
+
+if (day_week == 1) {
+    Console.WriteLine("Понедельник");
+} else if (day_week == 2) {
+Console.WriteLine("Вторник");
+} else if (day_week == 3) {
+Console.WriteLine("Среда");
+} else if (day_week == 4) {
+Console.WriteLine("Четверг");
+} else if (day_week == 5) {
+Console.WriteLine("Пятница");
+} else if (day_week == 6) {
+Console.WriteLine("Суббота");
+} else if (day_week == 7) {
+Console.WriteLine("Воскресенье");
+} else {
+    Console.WriteLine("Ошибка");
+}

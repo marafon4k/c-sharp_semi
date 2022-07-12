39f6d8653057a0410373428a8140e4cc5d5a088b
Igor Chichinin <marafon4k@gmail.com>
2022-07-12 22:31:55 +0300

    close project

---
+++ samostoyatelnaya_002/Program.cs
@@ -0,0 +1,34 @@
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли второе число
+// кратным первому. Если число 2 не кратно числу 1, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите 1е число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2е число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int sum = (number1 % number2);
+
+if (sum != 0) Console.WriteLine($"Не кратно, остаток {sum}");
+if (sum == 0) Console.WriteLine($"Кратно, остаток {sum}");
+
+
+
+
+// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum1 = number % 7;
+int sum2 = number % 23;
+
+if (sum1 == 0 && sum2 == 0) Console.WriteLine($"{number}, Да");
+if (sum1 != 0 || sum2 != 0) Console.WriteLine($"{number}, Нет");
--- task_09/Program.cs
+++ task_09/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int num = new Random().Next(10, 100);
+
+int MaxDigit(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+        if (firstDigit == secondDigit) return 0;
+        if (firstDigit > secondDigit) return firstDigit;
+        return secondDigit;
+}
+
+int maxDigit = MaxDigit(num);
+if (maxDigit == 0) Console.WriteLine("Цифры одинаковые.");
+else Console.WriteLine($"{num} -> {maxDigit}");
